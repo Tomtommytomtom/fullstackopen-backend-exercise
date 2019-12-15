fb310ea7745c0c5b9b8d1196bf0d55327bf3318e
Tom <tom.pillokat@googlemail.com>
2019-12-15 22:04:31 +0100

    Add cli tool for mongoDB cloud

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if( process.argv.length < 3 ){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const consoleArgs = process.argv
+
+const url =
+`mongodb+srv://tom:${password}@phonebook-db-72amz.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true})
+
+const personSchema = new mongoose.Schema({
+    name:   String,
+    number:  String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(consoleArgs.length == 3){
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+    mongoose.connection.close()
+    })
+} 
+else if(consoleArgs.length == 5){
+    const person = new Person({
+        name: consoleArgs[3],
+        number: consoleArgs[4]
+    })
+    person.save().then(result => {
+        console.log(person, ' saved!')
+        mongoose.connection.close()
+    })
+
+}
